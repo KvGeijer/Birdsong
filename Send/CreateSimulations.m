9a1b227d4b48577fa164fa86bb6f3d7dc1540f68
oskarastrom <oskar.astrom@yahoo.se>
2020-02-21 12:12:41 +0100

    Add files via upload
    Spectrogram settings and tripple simulation analysis

--- Send/CreateSimulations.m
+++ Send/CreateSimulations.m
@@ -1,22 +1,42 @@
 
-samples = 10000;
+samples = 1000;
 samplingTime = 1;
 imageSize = [75 100];
 
+% Spectrogram Paramters
+overlap = [.5, .9];
+n_win = [9, 1024];
+minFreqSize = [4096, 256];
+folderName = ["simulations_freq", "simulations_time", "simulations_sound"];
+
 for type = 1:6
+    for k = 1:3
+        if (~exist(folderName(k) + "/" + type, 'dir'))
+            mkdir(folderName(k), string(type));
+        end
+    end
     for n = 1:samples
         y = SimulateBird(type, samplingTime);
         
-        Ny = length(y);
-        nsc = floor(Ny/4.5);
-        nov = floor(nsc/2);
-        nff = max(256,2^nextpow2(nsc));
-        
-        img = abs(spectrogram(y,hamming(nsc),nov,nff));
-        maxVal = max(max(img));
-        minVal = min(min(img));
-        img = (img-minVal)/(maxVal-minVal);
-        imwrite(img,"simulations/" + type + "/" + n + ".png",'PNG');
+        for k = 1:2
+            Ny = length(y);
+            nsc = floor(Ny/(n_win(k) - overlap(k)*(n_win(k) - 1)));
+            nov = floor(nsc*(1 - overlap(k)));
+            nff = max(minFreqSize(k)*2,2^nextpow2(nsc));
+
+            img = abs(spectrogram(y,hamming(nsc),nov,nff));
+            maxVal = max(max(img));
+            minVal = min(min(img));
+            img = (img-minVal)/(maxVal-minVal);
+            imwrite(img, folderName(k) + "/" + type + "/" + n + ".png",'PNG');
+        end
+        for k = 3:3
+            img = y';
+            maxVal = max(max(img));
+            minVal = min(min(img));
+            img = (img-minVal)/(maxVal-minVal);
+            imwrite(img, folderName(k) + "/" + type + "/" + n + ".png",'PNG');
+        end
     end
     disp(type + " done!");
 end
--- Send/main.m
+++ Send/main.m
@@ -1,59 +1,68 @@
+paths = ["simulations_freq", "simulations_time", "simulations_sound"];
+nets = cell(1, 3);
+results_all = zeros(6, 6*3);
 
-imds = imageDatastore('simulations', 'IncludeSubfolders',true,'LabelSource','foldernames');
-%%
-figure;
-perm = randperm(size(imds.Files, 1),min(size(imds.Files, 1), 20));
-for i = 1:min(size(imds.Files, 1), 20)
-    subplot(4,5,i);
-    imshow(imds.Files{perm(i)});
+for i = 1:3
+    imds = imageDatastore(paths(i), 'IncludeSubfolders',true,'LabelSource','foldernames');
+    
+    labelCount = countEachLabel(imds);
+    imgSize = size(readimage(imds,1));
+    percentTrainFiles = 0.8;
+    numTrainFiles = round(percentTrainFiles * min(labelCount{:, 2}));
+    [imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+    
+    layers = [
+        imageInputLayer([imgSize(1) imgSize(2) 1])
+
+        convolution2dLayer(3,8,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2, 'Padding', 'same')
+
+        convolution2dLayer(3,16,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        maxPooling2dLayer(2,'Stride',2, 'Padding', 'same')
+
+        convolution2dLayer(3,32,'Padding','same')
+        batchNormalizationLayer
+        reluLayer
+
+        fullyConnectedLayer(size(labelCount, 1))
+        softmaxLayer
+        classificationLayer];
+
+    options = trainingOptions('sgdm', ...
+        'InitialLearnRate',0.01, ...
+        'MaxEpochs',10, ...
+        'Shuffle','every-epoch', ...
+        'ValidationData',imdsValidation, ...
+        'ValidationFrequency',30, ...
+        'Verbose',false, ...
+        'Plots','training-progress', ...
+        'ExecutionEnvironment', 'auto');
+
+    net = trainNetwork(imdsTrain,layers,options);
+    nets{1, i} = net;
+    
+    YPred = classify(nets{i},imdsValidation);
+    YValidation = imdsValidation.Labels;
+
+    results = zeros(size(labelCount, 1), size(labelCount, 1));
+    for kk = 1:length(YPred)
+        guess = str2num('uint8(' + string(YPred(kk)) + ')');
+        actual = str2num('uint8(' + string(YValidation(kk)) + ')');
+        results(guess, actual) = results(guess, actual) + 1;
+    end
+    results_all(:, (i-1)*6+1:(i-1)*6+6) = results;
 end
 %%
-labelCount = countEachLabel(imds);
-imgSize = size(readimage(imds,1));
-numTrainFiles = 8000;
-[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
-
-%%
-layers = [
-    imageInputLayer([imgSize(1) imgSize(2) 1])
-    
-    convolution2dLayer(3,8,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-    maxPooling2dLayer(2,'Stride',2)
-    
-    convolution2dLayer(3,16,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-    maxPooling2dLayer(2,'Stride',2)
-    
-    convolution2dLayer(3,32,'Padding','same')
-    batchNormalizationLayer
-    reluLayer
-    
-    fullyConnectedLayer(size(labelCount, 1))
-    softmaxLayer
-    classificationLayer];
-
-%%
-options = trainingOptions('sgdm', ...
-    'InitialLearnRate',0.0001, ...
-    'MaxEpochs',10, ...
-    'Shuffle','every-epoch', ...
-    'ValidationData',imdsValidation, ...
-    'ValidationFrequency',30, ...
-    'Verbose',false, ...
-    'Plots','training-progress', ...
-    'ExecutionEnvironment', 'auto');
-
-%%
-net = trainNetwork(imdsTrain,layers,options);
-
-%%
-YPred = classify(net,imdsValidation);
-YValidation = imdsValidation.Labels;
+k = 3;
+imds = imageDatastore(paths(k), 'IncludeSubfolders',true,'LabelSource','foldernames');
+YPred = classify(nets(k),imds);
+YValidation = imds.Labels;
 
 results = zeros(size(labelCount, 1), size(labelCount, 1));
 for i = 1:length(YPred)
@@ -63,12 +72,19 @@ for i = 1:length(YPred)
 end
 
 accuracy = sum(YPred == YValidation)/numel(YValidation);
+disp(results)
 
 %%
 index = randperm(size(imdsValidation.Files, 1),1);
 path = imdsValidation.Files{index};
 figure(1)
-imshow(path)
+%imshow(path)
 guess = string(classify(net, imread(path)));
 actual = string(imdsValidation.Labels(index));
 disp(actual + ": " + guess);
+%%
+for i = 1:min(labelCount{:, 2})
+    if (size(readimage(imds, i)) ~= imgSize) 
+        disp(size(readimage(imds, i)));
+    end
+end
